c0dd612766eb46ce00e2da150794641aed829312
namynam63 <104711080+namynam63@users.noreply.github.com>
2023-07-07 16:59:43 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -25,11 +25,11 @@
 class Config(object):
 
     # get a token from @BotFather
-    BOT_TOKEN = ""
+    BOT_TOKEN = " 6394139620:AAFmPQnnbJGpjneJWokk__9SFyCQNCWlaDc"
 
     # Get these values from my.telegram.org
-    API_ID = 1234567
-    API_HASH = ""
+    API_ID = 29681511 
+    API_HASH = " 645a6fd23087c6242625a1d2141dd884""
 
     # No need to change
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -40,11 +40,11 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3700
 
     # TG Ids
-    LOG_CHANNEL = -1007119867241
-    OWNER_ID = 1288398723
+    LOG_CHANNEL = -1001816169926 
+    OWNER_ID = 5687234987 
 
     # bot username without @
-    BOT_USERNAME = "AdvanceUrlUploaderBot"
+    BOT_USERNAME = " uploadcykmbot"
 
     # auth users
-    AUTH_USERS = [OWNER_ID]
+    AUTH_USERS = [ 5687234987 ]
